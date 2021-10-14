01a9b73a27fe681b5064d8356381255c2ef8dd93
ly-rgb <83774054+ly-rgb@users.noreply.github.com>
2021-10-14 17:34:50 +0800

    Update and rename README.md to Post_Bar.py

---
+++ Post_Bar.py
@@ -0,0 +1,21 @@
+import re
+import csv
+
+with open('/Users/momo/Desktop/source.txt','r',encoding='utf-8') as f:
+    source = f.read()
+
+result_list = []
+username_list = re.findall('username="(.*?)"',source,re.S)
+content_list = re.findall('j_d_post_content " style="display:;">(.*?)<',source,re.S)
+reply_time_list = re.findall('tail-info">(2017.*?)<',source,re.S)
+
+for i in range(len(username_list)):
+    result = {'username':username_list[i],
+              'content':content_list[i],
+              'reply_time':reply_time_list[i]}
+    result_list.append(result)
+
+with open('tieba.csv','w',encoding='utf-8') as f:
+    writer = csv.DictWriter(f,fieldnames=['username','content','reply_time'])
+    writer.writeheader()
+    writer.writerows(result_list)
